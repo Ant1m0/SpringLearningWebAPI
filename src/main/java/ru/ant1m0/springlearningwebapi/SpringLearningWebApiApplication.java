eea40b55602a2d0e9ba76b2d96638cd831cd7f20
Ant1m0 <helpwithnet@gmail.com>
2023-02-07 00:08:10 +0300

    Initial commit

---
+++ src/main/java/ru/ant1m0/springlearningwebapi/SpringLearningWebApiApplication.java
@@ -0,0 +1,13 @@
+package ru.ant1m0.springlearningwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ant1m0/springlearningwebapi/SpringLearningWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ant1m0.springlearningwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
